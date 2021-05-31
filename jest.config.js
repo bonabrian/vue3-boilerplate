2a823f7b12bbb4cd90aa5040ce8f4308253f6797
bonabrian <bonabriansiagian@gmail.com>
2021-05-31 17:11:05 +0700

    modify jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,33 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'vue'
+  ],
   transform: {
     '^.+\\.vue$': 'vue-jest',
+    '^.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest'
   },
+  moduleNameMapper: moduleNameMapper,
+  snapshotSerializers: [
+    'jest-serializer-vue'
+  ],
+  testMatch: [
+    '**/*.spec.(js|jsx|ts|tsx)',
+    '**/tests/**/*.spec.(js|jsx|ts|tsx)',
+    '**/__tests__/*.(js|jsx|ts|tsx)'
+  ],
+  testURL: 'http://localhost',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/components/**',
+    '**/pages/**',
+    '!**/node_modules/**'
+  ],
+  modulePathIgnorePatterns: [
+    '__snapshots__'
+  ],
+  preset: '@vue/cli-plugin-unit-jest'
 };
